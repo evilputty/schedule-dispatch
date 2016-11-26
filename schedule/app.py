db2d2f7bd61a5c7a9c812322da34c39d2a682968
Spencer Puthuff <sputhuff@gmail.com>
2016-11-25 18:18:11 -0800

    addition

--- schedule/app.py
+++ schedule/app.py
@@ -1,3 +1,12 @@
+'''
+Entry point for gunicorn to serve REST API
+'''
 import falcon
 
+import worker
+
+
 api = application = falcon.API()
+
+worker = worker.Resource()
+api.add_route('/worker', worker)
--- schedule/worker.py
+++ schedule/worker.py
@@ -1,2 +1,14 @@
+'''
+Module for handling worker requests with REST API
+'''
+
 import falcon
 
+
+class Resource(object):
+    '''
+    Class to contain worker resource object
+    '''
+    def on_get(self, req, resp):
+        resp.body = '{"body": "Hello World!"}'
+        resp.status = falcon.API()
